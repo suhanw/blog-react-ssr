37d7ca89bbe22f7b50acf7f78b767c8736f0c923
Suhan Wijaya <suhanw@gmail.com>
2021-05-31 22:03:25 -0400

    restore

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
+// ./server/index.js
+
 import React from 'react'
 import ReactDOMServer from 'react-dom/server'
 import express from 'express'
 import App from '../client/components/App'
 
 const app = express()
-const port = process.env.NODE_ENV === 'production' ? 8080 : 3000
-
-app.use('/build/client', express.static('build/client'))
+const port = 3000
 
 app.get('/', (req, res) => {
     const jsx = ReactDOMServer.renderToString(
@@ -14,17 +14,8 @@ app.get('/', (req, res) => {
         <App />
     )
 
-    // const clientBundleScript = `<script src="http://localhost:8080/scripts/bundle.js"></script>`; // [B]
-    // const clientBundleStyle = `<link rel="stylesheet" href="http://localhost:8080/styles/bundle.css">`; // [B]
-    const clientBundleScript =
-        process.env.NODE_ENV === 'production'
-            ? `<script src="build/client/scripts/bundle.js"></script>` // [B]
-            : `<script src="http://localhost:8080/scripts/bundle.js"></script>`
-
-    const clientBundleStyle =
-        process.env.NODE_ENV === 'production'
-            ? `<link rel="stylesheet" href="build/client/styles/bundle.css">` // [B]
-            : `<link rel="stylesheet" href="http://localhost:8080/styles/bundle.css">`
+    const clientBundleScript = `<script src="http://localhost:8080/scripts/bundle.js"></script>` // [B]
+    const clientBundleStyle = `<link rel="stylesheet" href="http://localhost:8080/styles/bundle.css">` // [B]
 
     res.send(`
         <!DOCTYPE html>
