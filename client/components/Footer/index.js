085c5e2f23fea6ae693c1c4812b848c0fc69c41a
Suhan Wijaya <suhanw@gmail.com>
2021-05-31 21:42:18 -0400

    prettier

--- client/components/Footer/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import style from './style';
-
-const Footer = () => (
-	<div className={style.footer}>Footer</div>
-);
-
-export default Footer;
--- client/components/Header/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import style from './style';
-
-const Header = () => (
-	<div className={style.header}>Header</div>
-);
-
-export default Header;
